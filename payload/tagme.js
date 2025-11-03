48ec3bf6d5ff61e4d6bce1ffd431466cd0b3858c
Newman <andrew.logan.newman@gmail.com>
2025-11-03 16:59:37 -0600

    Update tagme.js
    Whoops. Totally copied the wrong script.

--- payload/tagme.js
+++ payload/tagme.js
@@ -1,11 +1,6 @@
 /** @param {NS} ns */
 export async function main(ns) {
-  var target = ns.read("/payload/tag.txt")
-  //var target = ns.getHostname();
-  
-  while (true) {
-    await ns.grow(target)
-    await ns.weaken(target)
-    await ns.hack(target)
-  }
+  var tag = ns.getHostname();
+
+  ns.write("/payload/tag.txt", tag)
 }
